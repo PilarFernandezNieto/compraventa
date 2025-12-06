9138140ceb1885a8a1a07f3ac4707d9c13fbd9d5
PilarFernandezNieto <apreilmc@gmail.com>
2025-12-06 19:36:50 +0100

    Suppliers

---
+++ database/factories/SupplierFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Supplier>
+ */
+class SupplierFactory extends Factory
+{
+
+
+    protected $model = \App\Models\Supplier::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'ruc' => $this->faker->unique()->numerify('###########'),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => $this->faker->phoneNumber(),
+            'address' => $this->faker->address(),
+            'photo' => $this->faker->imageUrl(100, 100, 'people'),
+            'status' => $this->faker->randomElement(['ACTIVE', 'INACTIVE']),
+        ];
+    }
+}
